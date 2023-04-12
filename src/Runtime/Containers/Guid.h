ef0d278d3f7e81c808820269540f43a53ef4a9e9
Barış Üçkardeş <68399075+BarisUckardes@users.noreply.github.com>
2023-04-12 17:38:21 +0300

    Guid comments

--- src/Runtime/Containers/Guid.h
+++ src/Runtime/Containers/Guid.h
@@ -10,10 +10,32 @@ namespace Portakal
 	class PORTAKAL_API Guid final
 	{
 	public:
+		/// <summary>
+		/// Uses PlatfromGuid abstraction to create anew guid
+		/// </summary>
+		/// <returns></returns>
 		static Guid Create();
-		static String ToString(const Guid& id);
-		static Guid FromString(const String& text);
+
+		/// <summary>
+		/// Creates empty guid(same with default constructor)
+		/// </summary>
+		/// <returns></returns>
 		static Guid Zero();
+
+		/// <summary>
+		/// Converts guid to string
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns></returns>
+		static String ToString(const Guid& id);
+
+		/// <summary>
+		/// Returns Guid from string
+		/// </summary>
+		/// <param name="text"></param>
+		/// <returns></returns>
+		static Guid FromString(const String& text);
+
 	public:
 		Guid(unsigned int a, unsigned int b, unsigned int c, unsigned int d);
 		Guid();
