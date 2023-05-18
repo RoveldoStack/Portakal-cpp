bd1224062c0c5eec8361e2b16b20cf5bbabef6f5
Barış Üçkardeş <68399075+BarisUckardes@users.noreply.github.com>
2023-05-18 18:54:08 +0300

    Application icon settings. Win32 application icon setting implementation.

--- src/Runtime/Platform/PlatformIcon.h
+++ src/Runtime/Platform/PlatformIcon.h
@@ -4,12 +4,21 @@
 namespace Portakal
 {
 	class Window;
+
+	/// <summary>
+	/// Platform agnostic icon operations
+	/// </summary>
 	class PORTAKAL_API PlatformIcon
 	{
 	public:
 		PlatformIcon() = delete;
 		~PlatformIcon() = delete;
 
+		/// <summary>
+		/// Loads a icon from path.
+		/// </summary>
+		/// <param name="path"></param>
+		/// <param name="pTargetWindow"></param>
 		static void LoadIconFromPath(const String& path,Window* pTargetWindow);
 	};
 }
